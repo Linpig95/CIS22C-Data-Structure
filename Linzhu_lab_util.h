7ce57a3f684862da924aacd3187769741f034b90
Lin Zhu <18692274795@163.com>
2019-08-08 01:01:46 -0700

    finished lab 5

--- Linzhu_lab_util.h
+++ Linzhu_lab_util.h
@@ -53,7 +53,7 @@ public:
     bool is_empty() { return !head_; }
     EmptyState empty_state() { return empty_state_; }
     uint32_t size() { return size_; }
-    virtual T operator[] (uint32_t i) {
+    virtual T& operator[] (uint32_t i) {
         shared_ptr<ListNode<T>> p = head_;
         auto counter = 0;
         while (p && counter < i) {
@@ -241,3 +241,118 @@ ostream& operator<<(ostream& os, const Customer& c)
     os << c.firstName;
     return os;
 }
+
+template <class T>
+class DynamicArray : public DList<T>
+{
+public:
+    void push_back(T val) { this->append(make_shared<ListNode<T>>(val)); }
+    void push_front(T val) { this->prepend(make_shared<ListNode<T>>(val)); }
+    void pop_back() { this->remove(this->tail_); }
+    void pop_front() { this->remove(nullptr); }
+    bool empty() { return this->is_empty(); }
+    T& front() { return this->head_->value; }
+    T& back() { return this->tail_->value; }
+    void resize(int n)
+    {
+        int size = this->size();
+        if (size > n)
+        {
+            for (int i = 0; i < size - n; i++)
+            {
+                pop_back();
+            }
+        } else if (size < n)
+        {
+            for (int i = 0; i < n - size; i++)
+            {
+                push_back(T());
+            }
+        }
+    }
+};
+
+template <class T>
+class PriorityQueue
+{
+private:
+    int max_n_;
+    int n_;
+    DynamicArray<int> pq_;
+    DynamicArray<int> qp_;
+    DynamicArray<T> keys_;
+
+public:
+    PriorityQueue(int max_n)
+        : n_(0),
+          max_n_(max_n)
+    {
+        qp_.resize(max_n_ + 1);
+        pq_.resize(max_n_ + 1);
+        keys_.resize(max_n_ + 1);
+        for (int i = 0; i <= max_n_; i++)
+        {
+            qp_[i] = -1;
+        }
+    }
+    bool greater(int i, int j)
+    {
+        return keys_[pq_[i]] > keys_[pq_[j]];
+    }
+    void exch(int i, int j)
+    {
+        int swap = pq_[i];
+        pq_[i] = pq_[j];
+        pq_[j] = swap;
+        qp_[pq_[i]] = i;
+        qp_[pq_[j]] = j;
+    }
+    void swim(int k)
+    {
+        while (k > 1 && greater(k/2, k)) {
+            exch(k, k/2);
+            k /= 2;
+        }
+    }
+
+    void sink(int k)
+    {
+        while (2 * k <= n_)
+        {
+            int j = 2*k;
+            if (j < n_ && greater(j, j+1)) j++;
+            if (!greater(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
+    bool empty() { return n_ == 0; }
+    int size() { return n_; }
+    bool contains(int i) { return qp_[i] != -1; }
+    void push(int i, T key)
+    {
+        if (contains(i)) {
+            return;
+        }
+        n_++;
+        qp_[i] = n_;
+        pq_[n_] = i;
+        keys_[i] = key;
+        swim(n_);
+    }
+    int pop()
+    {
+        int min = pq_[1];
+        exch(1, n_--);
+        sink(1);
+        qp_[min] = -1;
+        keys_[min] = T();
+        pq_[n_+1] = -1;
+        return min;
+    }
+    void decrease_key(int i, T key)
+    {
+        keys_[i] = key;
+        swim(qp_[i]);
+    }
+};
--- linzhu_lab5_mst.cpp
+++ linzhu_lab5_mst.cpp
@@ -4,29 +4,231 @@
 #include <limits>
 #include <string>
 #include <utility>  
+#include <fstream>
+#include <unordered_map>
+#include <limits>
 using namespace std;
-using namespace lin;
+
+DynamicArray<string> cities;
+
+
+template <class T>
+class Edge
+{
+private:
+    int v_;
+    int w_;
+    T weight_;
+public:
+    Edge()
+        : v_(0),
+          w_(0),
+          weight_(T())
+    {}
+    Edge(int v, int w, T weight)
+        : v_(v),
+          w_(w),
+          weight_(weight)
+    {
+    }
+    Edge(const Edge<T> &other)
+        : v_(other.v_),
+          w_(other.w_),
+          weight_(other.weight_)
+    {
+    }
+    T weight() { return weight_; }
+    int either() { return v_; }
+    int other(int vertex) { return vertex == v_ ? w_ : v_; }
+    bool operator==(const Edge &other) { return other.weight_ == weight_; }
+    bool operator>(const Edge &other) { return other.weight_ > weight_; }
+    bool operator<(const Edge &other) { return other.weight_ < weight_; }
+
+};
+
+template <class T>
+class EdgeWeightedGraph
+{
+private:
+    int V_;
+    int E_;
+    DynamicArray<DynamicArray<Edge<T>>> adj_;
+public:
+    EdgeWeightedGraph(int V)
+        : V_(V),
+          E_(0)
+    {
+        for (int i = 0; i < V_; i++)
+        {
+            adj_.push_back(DynamicArray<Edge<T>>());
+        }
+    }
+    int V() { return V_; }
+    int E() { return E_; }
+    void add_edge(Edge<T> e)
+    {
+        int v = e.either();
+        int w = e.other(v);
+        adj_[v].push_back(e);
+        adj_[w].push_back(e);
+        E_++;
+    }
+    int degree(int vertex)
+    {
+        return adj_[vertex].size();
+    }
+    void set_E(int E) { E_ = E; }
+    DynamicArray<Edge<T>>& adj(int i)
+    {
+        return adj_[i];
+    }
+};
+
+template <class T>
+class PrimMST
+{
+private:
+    DynamicArray<Edge<T>> edge_to_;
+    DynamicArray<double> dist_to_;
+    DynamicArray<bool> marked_;
+    PriorityQueue<double> pq_;
+public:
+    void scan(EdgeWeightedGraph<T>& graph, int v)
+    {
+        marked_[v] = true;
+        for (int i = 0; i < graph.adj(v).size(); i++)
+        {
+            Edge<T> e = graph.adj(v)[i];
+            int w = e.other(v);
+            if (marked_[w]) continue;
+            if (e.weight() < dist_to_[w]) {
+                dist_to_[w] = e.weight();
+                edge_to_[w] = e;
+                if (pq_.contains(w)) pq_.decrease_key(w, dist_to_[w]);
+                else pq_.push(w, dist_to_[w]);
+            }
+        }
+    }
+    void prim(EdgeWeightedGraph<T>& graph, int s)
+    {
+        dist_to_[s] = 0.0;
+        pq_.push(s, dist_to_[s]);
+        while (!pq_.empty())
+        {
+            int v = pq_.pop();
+            scan(graph, v);
+        }
+    }
+    PrimMST(EdgeWeightedGraph<T>& graph)
+        : pq_(graph.V())
+    {
+        edge_to_.resize(graph.V());
+        dist_to_.resize(graph.V());
+        marked_.resize(graph.V());
+        for (int i = 0; i < graph.V(); i++)
+        {
+            dist_to_[i] = numeric_limits<double>::max();
+        }
+        for (int i = 0; i < graph.V(); i++)
+        {
+            if (!marked_[i]) prim(graph, i);
+        }
+    }
+    DynamicArray<Edge<T>> edges()
+    {
+        DynamicArray<Edge<T>> queue;
+        for (int v = 0; v < edge_to_.size(); v++)
+        {
+            Edge<T> e = edge_to_[v];
+            if (e.weight() > 0)
+                queue.push_back(e);
+        }
+        return queue;
+    }
+    double weight()
+    {
+        double weight = 0.0;
+        DynamicArray<Edge<T>> es = edges();
+        for (int i = 0; i < es.size(); i++)
+        {
+            weight += es[i].weight();
+        }
+        return weight;
+    }
+};
 
 int main()
 {
-    DynamicArray<int> array;
-    for (int i = 0; i < 10; i++)
-        array.push_back(i);
-    for (int i = 0; i < array.size(); i++)
+    unordered_map<string, int> city_vertex_mapping;
+    unordered_map<int, string> vertex_city_mapping;
+    ifstream fin("traveling_graph.txt");
+    int V;
+    fin >> V;
+    int E;
+    fin >> E;
+    int i = 0;
+    fin.get();
+    fin.get();
+    DynamicArray<Edge<int>> edges;
+    EdgeWeightedGraph<int> graph(V);
+    graph.set_E(E);
+    while (fin.good())
     {
-        cout << array[i] << ' ';
+        string city1;
+        getline(fin, city1, ',');
+        string city2;
+        getline(fin, city2, ',');
+        string weight_str;
+        getline(fin, weight_str, '\n');
+        int weight = stoi(weight_str);
+        cout << city1 << '\t' << city2 << '\t' << weight << endl;
+        int v, w;
+        if (city_vertex_mapping.find(city1) == city_vertex_mapping.end()) {
+            cout << city1 << " not found. Existing cities are" << endl;
+            for (auto city : city_vertex_mapping)
+            {
+                cout << city.first << ", ";
+            }
+            cout << endl;
+
+            city_vertex_mapping[city1] = i;
+            vertex_city_mapping[i] = city1;
+            v = i;
+            i++;
+            cout << i << " cities added" << endl;
+        } else {
+            v = city_vertex_mapping[city1];
+        }
+        if (city_vertex_mapping.find(city2) == city_vertex_mapping.end()) {
+            cout << city2 << " not found. Existing cities are" << endl;
+            for (auto city : city_vertex_mapping)
+            {
+                cout << city.first << ", ";
+            }
+            cout << endl;
+            city_vertex_mapping[city2] = i;
+            vertex_city_mapping[i] = city2;
+            w = i;
+            i++;
+            cout << i << " cities added" << endl;
+        } else {
+            w = city_vertex_mapping[city2];
+        }
+        edges.push_back(Edge<int>(v, w, weight));
+        graph.add_edge(edges.back());
     }
-    for (int i = 0; i < 10; i++)
+    PrimMST<int> mst(graph);
+    auto es = mst.edges();
+    cout << "MST is: " << endl;
+    for (int i = 0; i < es.size(); i++)
     {
-        cout << "popping " << array.back() << endl;
-        array.pop_back();
+        int v = es[i].either();
+        int w = es[i].other(v);
+        cout << "(" << vertex_city_mapping[v] << ", "
+             << vertex_city_mapping[w] << ", "
+             << es[i].weight() << ")" << endl;
     }
-    cout << endl;
-    cout << "after pop " << endl;
-    for (int i = 0; i < array.size(); i++)
-    {
-        cout << array[i] << ' ';
-    }
-    cout << endl;
+    cout << mst.weight() << endl;
     return 0;
+
 }
